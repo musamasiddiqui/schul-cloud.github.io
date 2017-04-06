6bfa18475d10260a86d772524608cd17e6eab24c
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-06 03:06:22 -0700

    debug print because travis hangs

--- tests/schul-cloud-spelling.sh
+++ tests/schul-cloud-spelling.sh
@@ -3,6 +3,10 @@
 cd "`dirname \"$0\"`"
 cd ..
 
+echo "In directory"
+pwd
+echo
+
 wrong_spelling_command="grep -n -riE 'schul(([^0-9a-zA-Z-])+|([[^:space:-]])*|[[:space:]]+-[[:space:]]*|[[:space:]]*-[[:space:]]+)cloud|shcul' | grep -vF '# fail'"
 
 if bash -c "$wrong_spelling_command"; then
