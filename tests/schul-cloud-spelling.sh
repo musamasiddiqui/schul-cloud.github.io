8c27ebf9d37dc689889ce675375b84eb03c39c09
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-06 03:10:56 -0700

    more output

--- tests/schul-cloud-spelling.sh
+++ tests/schul-cloud-spelling.sh
@@ -7,7 +7,7 @@ echo "In directory"
 pwd
 echo
 
-if grep -n -riE 'schul(([^0-9a-zA-Z-])+|([[^:space:-]])*|[[:space:]]+-[[:space:]]*|[[:space:]]*-[[:space:]]+)cloud|shcul' | grep -vF '# fail'
+if grep -n -riE 'schul(([^0-9a-zA-Z-])+|([[^:space:-]])*|[[:space:]]+-[[:space:]]*|[[:space:]]*-[[:space:]]+)cloud|shcul'
 then
   1>&2 echo -e "\e[1;31mMisspelling detected!\e[0m"
   exit 1
