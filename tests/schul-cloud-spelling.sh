a726f4f7f8772331f15a421f82ab2a89c35b9e65
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-04-06 02:26:37 -0700

    add spelling tests

---
+++ tests/schul-cloud-spelling.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "`dirname \"$0\"`"
+cd ..
+
+wrong_spelling_command="grep -n -riE 'schul(([^0-9a-zA-Z-])+|([[^:space:-]])*|[[:space:]]+-[[:space:]]*|[[:space:]]*-[[:space:]]+)cloud|shcul' | grep -vF '# fail'"
+wrong_spelling="`bash -c \"$wrong_spelling_command\"`"
+if [ -n "$wrong_spelling" ]; then
+  bash -c "$wrong_spelling_command"
+  1>&2 echo -e "\e[1;31mMisspelling detected!\e[0m"
+  exit 1
+fi
+
+1>&2 echo -e "\e[1;32mAll correct\e[0m"
+exit 0
